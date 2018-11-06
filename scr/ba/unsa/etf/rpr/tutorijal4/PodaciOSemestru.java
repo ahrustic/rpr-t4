d97236086205c68fcca95fae1332b88f39cfcfaf
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-11-06 01:56:40 +0100

    popunjena klasa PodaciOSemestru

--- scr/ba/unsa/etf/rpr/tutorijal4/PodaciOSemestru.java
+++ scr/ba/unsa/etf/rpr/tutorijal4/PodaciOSemestru.java
@@ -1,4 +1,60 @@
 package ba.unsa.etf.rpr.tutorijal4;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class PodaciOSemestru {
+    private Date datumUpisa, datumOvjere;
+    private int brojSemestra, smjerID;
+
+    public PodaciOSemestru(Date upis, Date ovjera, int broj, int id) {
+        this.datumUpisa = upis;
+        this.datumOvjere = ovjera;
+        this.brojSemestra = broj;
+        this.smjerID = id;
+    }
+
+    private ArrayList<Predmet> predmetiKojeSlusaju;
+
+
+
+    public void setDatumUpisa(Date datumUpisa) {
+        this.datumUpisa = datumUpisa;
+    }
+
+    public void setDatumOvjere(Date datumOvjere) {
+        this.datumOvjere = datumOvjere;
+    }
+
+    public void setBrojSemestra(int brojSemestra) {
+        this.brojSemestra = brojSemestra;
+    }
+
+    public void setPredmetiKojeSlusaju(ArrayList<Predmet> predmetiKojeSlusaju) {
+        this.predmetiKojeSlusaju = predmetiKojeSlusaju;
+    }
+
+    public Date getDatumUpisa() {
+        return datumUpisa;
+    }
+
+    public Date getDatumOvjere() {
+        return datumOvjere;
+    }
+
+    public int getBrojSemestra() {
+        return brojSemestra;
+    }
+
+    public int getSmjerID() {
+        return smjerID;
+    }
+
+    public ArrayList<Predmet> getPredmetiKojeSlusaju() {
+        return predmetiKojeSlusaju;
+    }
+
+
+
+
 }
