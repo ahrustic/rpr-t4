0517ff0ca39fce793afd06c85dc1099422b51305
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-11-11 11:40:54 +0100

    doradena klasa Upis

--- scr/ba/unsa/etf/rpr/tutorijal4/Upis.java
+++ scr/ba/unsa/etf/rpr/tutorijal4/Upis.java
@@ -1,15 +1,28 @@
 package ba.unsa.etf.rpr.tutorijal4;
 
-import java.util.ArrayList;
-import java.util.Set;
+import java.util.List;
 
 public class Upis {
 
-        private ArrayList<Predmet> predmeti;
+        private Predmet predmet;
         private PlanStudija planStudija;
         private Student student;
 
     public Upis(Predmet predmet, Student student, PlanStudija planStudija) {
+            this.predmet = predmet;
+            this.student = student;
+            this.planStudija = planStudija;
+    }
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public PlanStudija getPlanStudija() {
+        return planStudija;
+    }
+
+    public Predmet getPredmeti() {
+        return predmet;
     }
 }
